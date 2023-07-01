fd77dc70f143da09efcb11d07c91a074ad7b3f57
0x2447196 <2447196@gmail.com>
2023-07-01 00:16:31 -0400

    basic functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ async function createDB() {
     schema: {
       product: 'string',
       description: 'string',
-      ingredients: 'string[]'
+      ingredients: 'string'
     },
     components: {
       // Register the hook
@@ -20,16 +20,50 @@ async function createDB() {
     }
   })
 
-  await insertMultiple(db, data, 500)
+  const searchData = data.map(d => ({
+    ...d,
+    ingredients: d.ingredients.join(", ")
+  }))
+
+  await insertMultiple(db, searchData, 500)
 
   return db;
 }
 
 
+const HighlightedSearchResult = ({ matches, original }) => {
+  const resultString = Object.keys(matches)[0];
+  const highlights = matches[resultString];
+
+  if (!highlights) { return original }
+
+  let currentIndex = 0;
+  let parts = [];
+
+  highlights.forEach((highlight) => {
+    const { start, length } = highlight;
+    const normal = original.slice(currentIndex, start);
+    const highlighted = original.slice(start, start + length);
+
+    parts.push(<span>{normal}</span>);
+    parts.push(<span style={{ backgroundColor: 'yellow' }}>{highlighted}</span>);
+
+    currentIndex = start + length;
+  });
+
+  // Add the remaining text, if any
+  if (currentIndex < original.length) {
+    const remaining = original.slice(currentIndex);
+    parts.push(<span>{remaining}</span>);
+  }
+
+  return <>{parts}</>;
+};
 
 export default function Home() {
   const db = useRef()
   const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
     async function loadDb() {
@@ -44,17 +78,15 @@ export default function Home() {
     async function searchDB() {
       const normalizedQuery = searchQuery.toLowerCase().trim();
 
-      if (db.current && normalizedQuery) {
-        const searchResult = await search(db.current, {
-          term: normalizedQuery,
-          properties: '*',
-        })
+      if (db.current) {
         const results = await searchWithHighlight(db.current, {
           term: normalizedQuery,
-          properties: '*',
+          properties: ['product', 'ingredients', 'description'],
         })
-
         console.log(results)
+
+        setSearchResults(results.hits)
+
       }
     }
 
@@ -64,13 +96,55 @@ export default function Home() {
 
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <>
+     <div className="fixed top-0 left-0 right-0 z-50 bg-white p-2">
+      <div className="max-w-3xl mx-auto">
+      <h1 className="font-bold">IdeaLabs Search</h1>
       <input 
-      type="text" 
-      value={searchQuery}
-       onChange={e => setSearchQuery(e.target.value)}
+        type="text" 
+        value={searchQuery}
+        onChange={e => setSearchQuery(e.target.value)}
+        className="w-full p-2 mb-4 border rounded"
       />
+      </div>
+
+    </div>
+    <main className="max-w-3xl mx-auto py-4 mt-20">
+      <table className="w-full divide-y divide-gray-200">
+        <thead className="bg-gray-50">
+          <tr>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Product
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Ingredients
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Description
+            </th>
+          </tr>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          {searchResults.map((result) => (
+            <tr key={result.id}>
+              <td className="px-6 py-4  whitespace-normal overflow-auto align-top">
+                <HighlightedSearchResult original={result.document.product} matches={result.positions.product} />
+              </td>
+              <td className="px-6 py-4  whitespace-normal overflow-auto align-top">
+                <HighlightedSearchResult original={result.document.ingredients} matches={result.positions.ingredients} />
+              </td>
+              <td className="px-6 py-4  whitespace-normal overflow-auto align-top">
+                <div className="max-h-56 overflow-y-scroll">
+                <HighlightedSearchResult original={result.document.description} matches={result.positions.description} />
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </main>
-  )
+    </>
+
+  )  
 }
 
